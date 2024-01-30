818dc2bdf4c304667e2ee48b184119baf6fe4755
Davron17 <dorazaliyev75@gmail.com>
2024-01-30 16:22:02 +0500

    echo_text

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask,request
 import telegram
-
-TOKEN='6440042136:AAGp50G4rRElB42wkDg7DlM91-opDBQwjhc'
+# Create the application instance
+TOKEN ='6440042136:AAGp50G4rRElB42wkDg7DlM91-opDBQwjhc'
 bot = telegram.Bot(TOKEN)
-chat_id='5271463532'
+# chat_id = '5271463532'
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/',methods=["POST"])
 def index():
-    bot.send_message(chat_id,text='Hello world')
-    return "Hello"
+    data=request.get_json()
+    print(data)
+    bot.send_message(chat_id=data['message']['chat']['id'],text=data['message']['text'])
+    return 'index page'
 
-if __name__=='__main__':
+if __name__=="__main__":
     app.run(debug=True)
